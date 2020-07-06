43e1838ee645bb6fa6d84690eb03af7e8e35f0c3
Hari Wicaksono <hariwicaksono87@gmail.com>
2020-07-06 15:43:21 +0700

    Update Index.js

--- src/Component/User/Index.js
+++ src/Component/User/Index.js
@@ -34,10 +34,9 @@ class Index extends Component {
                         
                         <Col md="12">
                         <CardDeck>
-                        <Card>
-                            
+                        <Card bg="primary" text="white">
                             <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                            <Card.Title className="text-white">Card title</Card.Title>
                             <Card.Text>
                                 This is a wider card with supporting text below as a natural lead-in to
                                 additional content. This content is a little bit longer.
